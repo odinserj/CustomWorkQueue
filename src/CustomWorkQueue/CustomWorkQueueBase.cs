f75c271c32248b557df4eb808f06340da15f0010
Sergey Odinokov <odinserj@gmail.com>
2020-05-26 16:40:09 +0300

    Update CustomWorkQueueBase.cs

--- src/CustomWorkQueue/CustomWorkQueueBase.cs
+++ src/CustomWorkQueue/CustomWorkQueueBase.cs
@@ -123,7 +123,7 @@ namespace CustomWorkQueue
 
             int c = _globalQueues.Length - 1;
 
-            int maxIndex = c - 1;
+            int maxIndex = c;
             int i = locals.PreferredIndex;
             while (c > 0)
             {
